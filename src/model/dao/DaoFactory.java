d0cf72db11280754964f54e66e0c0bd3f90888f1
Paulo henrique <paulo1995paulo@hotmail.com>
2022-09-27 15:03:34 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 // a minha clase vai expor um metodo que retorna o tipo da interface mas
@@ -11,4 +12,8 @@ public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC (DB.getConnection());
 	}
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
+
 }
