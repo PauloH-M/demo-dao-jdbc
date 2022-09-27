dc84c31622370214302c1acee381eb401977a86f
Paulo henrique <paulo1995paulo@hotmail.com>
2022-09-27 13:35:10 -0300

    findByDepartment implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -11,4 +12,6 @@ public interface SellerDao {
 	void deleteById(Integer id);//responsavel por deletar no banco de dados o obj do comando de entrada
 	Seller findById(Integer id);//responsavel por consultar no banco de dados o obj e retornar do comando de entrada
 	List<Seller> findAll();//responsavel por retornar todo o banco de dados
+    List<Seller> findByDepartment(Department department);
+
 }
